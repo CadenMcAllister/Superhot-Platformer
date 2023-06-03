e7121168d965406c46e1ca1fae0ecb63268663bc
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-06-03 11:11:50 -0500

    Game Feel
    There are now sound effects and particles when the projectiles his something.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject bulletObject;
-
+    public GameObject ExplosionObject;
+    public AudioSource ExplosionAudioSource;
+    public AudioClip ExplosionAudioClip;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
 private void OnCollisionEnter2D(Collision2D collision){
     if (collision.gameObject.tag == "cube" | collision.gameObject.tag == "Player" | collision.gameObject.tag == "Ground" | collision.gameObject.tag == "Projectile"){
-        Destroy(gameObject);
+        ExplosionAudioSource.Play();
+        Instantiate(ExplosionObject, transform.position, transform.rotation);
+        Invoke("Explosion", 1);
     }
 }
 
+
+
+    public void Explosion(){
+        Destroy(gameObject);
+    }
 }
